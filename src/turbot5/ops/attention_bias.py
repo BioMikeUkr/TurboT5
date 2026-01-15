T5EncoderForMaskedLM aded; mask broadcasting fixed; d bias casting to fp32 added

src/turbot5/ops/attention_bias.py
@@ -119,7 +119,9 @@ def bias_kernel_backward(
         d_out_values = tl.load(d_out_ptrs, mask=o_mask, other=0.0)
 
         d_weights_ptrs = d_weights + bucket_offs
-        tl.atomic_add(d_weights_ptrs, d_out_values, mask=relative_buckets[:, :, None] < NUM_BUCKETS)
+        # BF16 is not supported in atomic_add, cast to FP32
+        d_out_values_atomic = d_out_values.to(tl.float32)
+        tl.atomic_add(d_weights_ptrs, d_out_values_atomic, mask=relative_buckets[:, :, None] < NUM_BUCKETS)
 
 
 class BiasOp(torch.autograd.Function):
@@ -155,8 +157,9 @@ class BiasOp(torch.autograd.Function):
         M, N, NH = ctx.M, ctx.N, ctx.NH
         BIDIRECTIONAL, NUM_BUCKETS, MAX_DISTANCE = ctx.BIDIRECTIONAL, ctx.NUM_BUCKETS, ctx.MAX_DISTANCE
         dtype = ctx.dtype
-
-        d_weights = torch.zeros_like(weights)
+        
+        # bf16 gradients are accumulated in float32
+        d_weights = torch.zeros_like(weights, dtype=torch.float32)
 
         # Config
         BLOCK_SIZE_N = 32

src/turbot5/ops/varlen_fused_bias_attn.py
@@ -118,8 +118,9 @@ def flash_attn_v2_bwd_bias(o, do, q, k, v, bias_weights, L,
     dk = torch.empty_like(k)
     dv = torch.empty_like(v)
 
+    # bf16 is not supported in atomic_add, so we always use fp32 for db
     if has_bias:
-        db = torch.zeros((NUM_BUCKETS, H), dtype=bias_weights.dtype, device=bias_weights.device)
+        db = torch.zeros((NUM_BUCKETS, H), dtype=torch.float32, device=bias_weights.device)
     else:
         db = None
 
@@ -611,7 +612,9 @@ def _bwd_kv_bias_kernel(
         # calculate dw
         if HAS_BIAS:
             db_ptrs = DB+bucket_offs
-            tl.atomic_add(db_ptrs, ds, mask=relative_buckets < NUM_BUCKETS)
+            # BF16 is not supported in atomic_add, always cast to FP32
+            ds_atomic = ds.to(tl.float32)
+            tl.atomic_add(db_ptrs, ds_atomic, mask=relative_buckets < NUM_BUCKETS)
 
         # increment pointers
         q_ptrs += BLOCK_M * stride_qz